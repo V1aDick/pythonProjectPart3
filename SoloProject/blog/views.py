4513984e063741aa60aa63ec24aad26e65f3e9b8
V1Dick <djecom.20@mail.ru>
2022-09-20 00:30:15 +0500

    x

--- SoloProject/blog/views.py
+++ SoloProject/blog/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.views.generic import ListView, DetailView
 from .models import Post
-from django.views.generic.edit import CreateView
+from django.views.generic.edit import CreateView, UpdateView
 # Create your views here.
 
 class BlogListView(ListView):
@@ -16,3 +16,8 @@ class BlogCreateView(CreateView):
     model = Post
     template_name = 'post_new.html'
     fields = '__all__'
+
+class BlogUpdateView(UpdateView):
+    model = Post
+    fields = ['title', 'body']
+    template_name = 'post_edit.html'
